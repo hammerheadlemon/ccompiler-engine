1d469b5c9313828621899ffc75b8599fb094b3f6
Matthew Lemon <lemon@matthewlemon.com>
2019-11-29 21:33:53 +0000

    simpler implementation to print cell values

--- main.c
+++ main.c
@@ -15,6 +15,24 @@ int list_sheets_callback(const char *sheetname, void *callbackdata) {
     return 0;
 }
 
+
+//callback data structure
+struct xlsx_callback_data {
+    char *sheetname;
+};
+
+
+int rowcallback(size_t row, size_t maxcol, void* callbackdata) {
+    printf("In BUT THIS IS SELDOM USED!\n");
+    return 0;
+}
+
+int sheet_cell_callback(size_t row, size_t maxcol, const char* value,  void* callbackdata) {
+    struct xlsx_callback_data *data = (struct xlsx_callback_data *) callbackdata;
+    printf("Sheet %s Row %ld, Col %ld Value %s\n", data->sheetname, row, maxcol, value);
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     char **sheets = malloc(sizeof(char*) * 20);
@@ -37,6 +55,9 @@ int main(int argc, char *argv[])
     printf("First entry in sheets in main after xlsxioread_list_sheets is %s\n", sheets[0]);
     for (int i=0; i < 5; i++) { // 5 is wrong here
         printf("%s\n", sheets[i]);
+        struct xlsx_callback_data callbackdata;
+        callbackdata.sheetname = sheets[i];
+        xlsxioread_process(reader, sheets[i], XLSXIOREAD_SKIP_EMPTY_ROWS, sheet_cell_callback, rowcallback, &callbackdata);
     }
     return 0;
 }
